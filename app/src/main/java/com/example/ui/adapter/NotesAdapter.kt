e878290b9c9cf79f7326b1538fc254e86ed21d1d
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-12-07 11:05:01 +0530

    add color for cardLayout

--- app/src/main/java/com/example/ui/adapter/NotesAdapter.kt
+++ app/src/main/java/com/example/ui/adapter/NotesAdapter.kt
@@ -1,34 +1,39 @@
 package com.example.ui.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.data.Note
 import com.example.notesapp.databinding.ListItemBinding
-import com.example.uitls.randomColor
+import com.example.ui.NoteBottomSheetFragment
+import com.example.uitls.NoteColor
 
 class NotesAdapter(private val listener: NotesItemClickListener) :
     RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
-
     private val notesList = ArrayList<Note>()
+    var noteBottomSheetFragment: NoteBottomSheetFragment? = null
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return NoteViewHolder(binding)
     }
 
+    @SuppressLint("ResourceType")
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val currentNote = notesList[position]
         with(holder.binding) {
             tvTitle.text = currentNote.title
             tvNote.text = currentNote.noteDec
             tvDate.text = currentNote.date
+            currentNote.color?.let { cardLayout.setBackgroundColor(it) }
 
-            cardLayout.setCardBackgroundColor(
-                holder.itemView.resources.getColor(randomColor(), null)
-            )
-            //setting up Listeners
+            noteBottomSheetFragment = NoteBottomSheetFragment { noteColor ->
+                cardLayout.setBackgroundColor(noteColor.color)
+            }
+
             cardLayout.setOnClickListener {
                 listener.onItemClicked(notesList[holder.adapterPosition])
             }
