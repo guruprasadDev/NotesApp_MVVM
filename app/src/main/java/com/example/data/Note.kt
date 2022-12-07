d46da3ab90cc1d2d6803d5955575ab3e030cbc23
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-12-07 10:55:05 +0530

    Add Color in Note data class

--- app/src/main/java/com/example/data/Note.kt
+++ app/src/main/java/com/example/data/Note.kt
@@ -3,11 +3,13 @@ package com.example.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.uitls.NoteColor
 
 @Entity(tableName = "notes_table")
 data class Note(
     @PrimaryKey(autoGenerate = true) var id: Int = 0,
     @ColumnInfo(name = "title") val title: String = "",
     @ColumnInfo(name = "noteDec") val noteDec: String = "",
-    @ColumnInfo(name = "date") val date: String = ""
+    @ColumnInfo(name = "date") val date: String = "",
+    @ColumnInfo(name = "color") var color: Int?
 ) : java.io.Serializable
