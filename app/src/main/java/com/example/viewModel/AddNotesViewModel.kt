da253b5dc16b97ded3b3e49b04dcaae77e58e840
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-12-09 12:14:55 +0530

    create liveData for NoteColor and fun onColorChanged()

--- app/src/main/java/com/example/viewModel/AddNotesViewModel.kt
+++ app/src/main/java/com/example/viewModel/AddNotesViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.uitls.NoteColor
 
 class AddNotesViewModel : ViewModel() {
+    val noteColorLiveData = MutableLiveData<NoteColor>()
+
+    fun onColorChanged(noteColor: NoteColor) {
+        noteColorLiveData.value = noteColor
+    }
+
     fun validateNote(title: String, noteMessage: String): Boolean {
         return title.isNotEmpty() || noteMessage.isNotEmpty()
     }
