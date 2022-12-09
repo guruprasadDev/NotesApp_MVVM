5468d3088e6827b718dcd35443f1b619f47105ae
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-12-09 17:11:04 +0530

    remove @ColumnInfo not required any more

--- app/src/main/java/com/example/data/Note.kt
+++ app/src/main/java/com/example/data/Note.kt
@@ -1,15 +1,13 @@
 package com.example.data
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.example.uitls.NoteColor
 
 @Entity(tableName = "notes_table")
 data class Note(
     @PrimaryKey(autoGenerate = true) var id: Int = 0,
-    @ColumnInfo(name = "title") val title: String = "",
-    @ColumnInfo(name = "noteDescription") val noteDescription: String = "",
-    @ColumnInfo(name = "date") val date: String = "",
-    @ColumnInfo(name = "color") var color: String
+    val title: String = "",
+    val noteDescription: String = "",
+    val date: String = "",
+    var color: String
 ) : java.io.Serializable
