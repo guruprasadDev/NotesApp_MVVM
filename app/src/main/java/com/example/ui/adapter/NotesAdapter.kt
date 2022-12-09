21fb433553141949d7d2e33ada77944e1eef0072
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-12-09 12:25:43 +0530

    create bind() function inside ViewHolder

--- app/src/main/java/com/example/ui/adapter/NotesAdapter.kt
+++ app/src/main/java/com/example/ui/adapter/NotesAdapter.kt
@@ -1,50 +1,31 @@
 package com.example.ui.adapter
 
-import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.data.Note
 import com.example.notesapp.databinding.ListItemBinding
-import com.example.ui.NoteBottomSheetFragment
-import com.example.uitls.NoteColor
 
 class NotesAdapter(private val listener: NotesItemClickListener) :
     RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+    private lateinit var binding: ListItemBinding
     private val notesList = ArrayList<Note>()
-    var noteBottomSheetFragment: NoteBottomSheetFragment? = null
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return NoteViewHolder(binding)
     }
 
-    @SuppressLint("ResourceType")
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val currentNote = notesList[position]
-        with(holder.binding) {
-            tvTitle.text = currentNote.title
-            tvNote.text = currentNote.noteDec
-            tvDate.text = currentNote.date
-            currentNote.color?.let { cardLayout.setBackgroundColor(it) }
+        holder.bind(currentNote)
+    }
 
-            noteBottomSheetFragment = NoteBottomSheetFragment { noteColor ->
-                cardLayout.setBackgroundColor(noteColor.color)
-            }
-
-            cardLayout.setOnClickListener {
-                listener.onItemClicked(notesList[holder.adapterPosition])
-            }
-            cardLayout.setOnLongClickListener {
-                listener.onLongItemClicked(
-                    notesList[holder.adapterPosition],
-                    cardLayout
-                )
-                true
-            }
-        }
+    private fun updateBackgroundColor(color: String) {
+        val noteBgColor = Color.parseColor(color)
+        binding.cardLayout.setBackgroundColor(noteBgColor)
     }
 
     override fun getItemCount() = notesList.size
@@ -55,7 +36,28 @@ class NotesAdapter(private val listener: NotesItemClickListener) :
         notifyDataSetChanged()
     }
 
-    inner class NoteViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class NoteViewHolder(private val binding: ListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(currentNote: Note) {
+            with(binding) {
+                tvTitle.text = currentNote.title
+                tvNote.text = currentNote.noteDescription
+                tvDate.text = currentNote.date
+                updateBackgroundColor(currentNote.color)
+
+                cardLayout.setOnClickListener {
+                    listener.onItemClicked(notesList[adapterPosition])
+                }
+                cardLayout.setOnLongClickListener {
+                    listener.onLongItemClicked(
+                        notesList[adapterPosition],
+                        cardLayout
+                    )
+                    true
+                }
+            }
+        }
+    }
 
     interface NotesItemClickListener {
         fun onItemClicked(note: Note)
