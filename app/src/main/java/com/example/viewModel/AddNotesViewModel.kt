144ec8862dbf8dd207d94e04c519618524438185
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-12-09 17:10:20 +0530

    change public to private _noteColorLiveData

--- app/src/main/java/com/example/viewModel/AddNotesViewModel.kt
+++ app/src/main/java/com/example/viewModel/AddNotesViewModel.kt
@@ -1,14 +1,18 @@
 package com.example.viewModel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.uitls.NoteColor
 
 class AddNotesViewModel : ViewModel() {
-    val noteColorLiveData = MutableLiveData<NoteColor>()
+    private val _noteColorLiveData = MutableLiveData<NoteColor>()
+    val noteColorLiveData: LiveData<NoteColor>
+        get() = _noteColorLiveData
+
 
     fun onColorChanged(noteColor: NoteColor) {
-        noteColorLiveData.value = noteColor
+        _noteColorLiveData.value = noteColor
     }
 
     fun validateNote(title: String, noteMessage: String): Boolean {
