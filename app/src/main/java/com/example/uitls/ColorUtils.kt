a26d8c62e3b36dc210557aed36ab0b3339237ac2
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-12-09 12:21:27 +0530

    change ColorUtils.kt file using string as color

--- app/src/main/java/com/example/uitls/ColorUtils.kt
+++ app/src/main/java/com/example/uitls/ColorUtils.kt
@@ -1,16 +1,14 @@
 package com.example.uitls
 
-import android.graphics.Color
-
-enum class NoteColor(val color: Int) {
-    WHITE(Color.parseColor("#FFFFFF")),
-    BLACK(Color.parseColor("#000000")),
-    SILVER(Color.parseColor("#C0C0C0")),
-    GRAY(Color.parseColor("#808080")),
-    MAROON(Color.parseColor("#800000")),
-    RED(Color.parseColor("#FF0000")),
-    FUCHSIA(Color.parseColor("#FF00FF")),
-    GREEN(Color.parseColor("#008000")),
-    LIME(Color.parseColor("#00FF00")),
-    BLUE(Color.parseColor("#03A9F4")),
+enum class NoteColor(val color: String) {
+    WHITE("#FFFFFF"),
+    BLACK("#000000"),
+    SILVER("#C0C0C0"),
+    GRAY("#808080"),
+    MAROON("#800000"),
+    RED("#FF0000"),
+    FUCHSIA("#FF00FF"),
+    GREEN("#008000"),
+    LIME("#00FF00"),
+    BLUE("#03A9F4"),
 }
